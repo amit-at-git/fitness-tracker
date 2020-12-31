a987da549e12623c58b955cbb0e6cc361835ccda
Amit Chauhan <chauhan_amit@yahoo.com>
2020-12-30 22:58:49 -0500

    Created basic components and routing rules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSliderModule } from '@angular/material/slider';
-
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatButtonModule
   ],
-  exports: [MatSliderModule]
+  exports: [
+    MatSliderModule,
+    MatButtonModule
+  ]
 })
 export class MaterialModule { }
